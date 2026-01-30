b3e26f71249f9a56c292250cd8cc4d4d7ea50fee
CedricDelaCruz <cedricgabrielr@gmail.com>
2026-01-31 02:34:35 +0800

    NEW DIRECTION | 2:34am

--- syntax-game/Player/Scripts/player.gd
+++ syntax-game/Player/Scripts/player.gd
@@ -5,7 +5,7 @@ var direction : Vector2 = Vector2.ZERO
 var move_speed : float = 100.0
 var state : String = "idle"
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayerdddd
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
 
@@ -16,7 +16,6 @@ func _ready():
 func _process( delta ):
 	 
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	velocity = direction * move_speed
 	
